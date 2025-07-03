43530baf8f15b556b3ff88beb6d83508b72aa506
RAKSHALI M A <119483785+RAKSHALI08@users.noreply.github.com>
2025-07-03 12:03:30 +0530

    Add files via upload

---
+++ week 3/Spring-data-jpa-handson/Difference between JPA Hibernate and Spring Data JPA/EmployeeManagement/src/main/java/com/example/employeemanagement/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.example.employeemanagement.service;
+
+import com.example.employeemanagement.model.Employee;
+import com.example.employeemanagement.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Transactional
+    public void addEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+}
