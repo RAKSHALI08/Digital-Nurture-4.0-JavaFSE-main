6cd855603f924b69a5ba5699d6b54ae0bebe824e
RAKSHALI M A <119483785+RAKSHALI08@users.noreply.github.com>
2025-07-12 13:37:54 +0530

    Add files via upload

---
+++ week 4/Spring REST using Spring Boot/5. JWT-handson/Create authentication service that returns JWT/auth-service/src/main/java/com/example/auth/util/JwtUtil.java
@@ -0,0 +1,25 @@
+
+package com.example.auth.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "secret";
+
+    public String generateToken(String username) {
+        long expirationTime = 1000 * 60 * 10; // 10 minutes
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
