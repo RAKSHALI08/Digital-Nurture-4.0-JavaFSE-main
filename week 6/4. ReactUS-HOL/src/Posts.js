c5cc4793322e988f96906fb73bd28637cf5491d4
RAKSHALI M A <119483785+RAKSHALI08@users.noreply.github.com>
2025-07-27 13:16:27 +0530

    Add files via upload

---
+++ week 6/4. ReactUS-HOL/src/Posts.js
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  loadPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => this.setState({ posts: data }))
+      .catch((error) => {
+        console.error('Error loading posts:', error);
+        alert('Failed to load posts.');
+      });
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>All Blog Posts</h2>
+        {this.state.posts.map((post) => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
